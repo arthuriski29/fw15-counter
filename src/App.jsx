41f807ee86d6c2a7adf1c6055ce0c836f53c8a53
arthuriski29 <78630365+arthuriski29@users.noreply.github.com>
2023-05-03 17:06:10 +0700

    inserting layout counter in html

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,15 @@ function App(){
   return(
     <>
       <div className='flex h-screen justify-center items-center'>
-        <h1>10</h1>
+        <div className= 'flex flex-col gap-7'>
+          <div className= 'text-center text-2xl'>
+            <h1>0</h1>
+          </div>
+          <div className= 'flex gap-16'>
+            <button className="btn btn-outline text-xl">-</button>
+            <button className="btn btn-outline text-xl">+</button>
+          </div>
+        </div>
       </div>
     </>
   )
