0adc354307572f47435718f53c4fdb8795e81d63
arthuriski29 <78630365+arthuriski29@users.noreply.github.com>
2023-05-04 11:14:16 +0700

    setconfig : theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,5 +10,22 @@ export default {
   plugins: [
     require("daisyui")
   ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#DED6C6",
+          "secondary": "#FFBA7B",
+          "accent": "#AA7C52",
+          "neutral": "#191D24",
+          "base-100": "#2A303C",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  }
 }
 
